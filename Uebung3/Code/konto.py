f51c256414fd80e436131f179b303c097c1d68af
Daniel Wilming <wilming.daniel@fh-swf.de>
2025-10-11 09:41:11 +0000

    Implementierung der Konto-Klasse und ihrer vorgegebenen Methoden aus dem Interface.

--- Uebung3/Code/konto.py
+++ Uebung3/Code/konto.py
@@ -5,7 +5,7 @@ TODO: Implementieren Sie die Konto-Klasse basierend auf dem KontoInterface
 """
 
 from decimal import Decimal
-from .interfaces import KontoInterface
+from interfaces import KontoInterface
 
 class Konto(KontoInterface):
     """
@@ -18,23 +18,37 @@ class Konto(KontoInterface):
     """
     
     def __init__(self, konto_id: int, saldo: Decimal = Decimal('0.00')):
-        # TODO: Implementierung
-        pass
+        if konto_id is None or not isinstance(konto_id, int):
+            raise ValueError("konto_id muss eine numerische ID sein und darf nicht leer sein.")
+        if saldo < Decimal('0.00'):
+            raise ValueError("Startsaldo darf nicht negativ sein.")
+        self._konto_id = konto_id
+        self._saldo = saldo
     
     @property
     def konto_id(self) -> int:
-        # TODO: Implementierung
-        pass
+        return self._konto_id
     
     @property
     def saldo(self) -> Decimal:
-        # TODO: Implementierung
-        pass
+        return self._saldo
     
     def einzahlen(self, betrag: Decimal) -> None:
-        # TODO: Implementierung
-        pass
-    
+        if betrag is None:
+            raise ValueError("Leere Einzahlungen sind nicht erlaubt.")
+        if betrag < Decimal('0.00'):
+            raise ValueError("Konto darf keine negative Einzahlung erhalten.")
+        
+        self._saldo += betrag
+
     def auszahlen(self, betrag: Decimal) -> None:
         # TODO: Implementierung
-        pass
+
+        if betrag is None:
+            raise ValueError("Leere Auszahlungen sind nicht erlaubt.")
+        if betrag < Decimal('0.00'):
+            raise ValueError("Konto darf keine negative Auszahlung erhalten.")
+        if self._saldo - betrag < Decimal('0.00'):
+            raise ValueError("Konto darf nicht überzogen werden.")
+
+        self._saldo -= betrag
