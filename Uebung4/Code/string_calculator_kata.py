709315c551b98feaca5f0619405cec92ec199a32
Roman <85564248+rh0-me@users.noreply.github.com>
2025-11-13 17:52:27 +0000

    TeamB: Green: Handling negative numbers

--- Uebung4/Code/string_calculator_kata.py
+++ Uebung4/Code/string_calculator_kata.py
@@ -7,16 +7,30 @@ def add(numbers: str) -> int:
         numbers = numbers.replace("\n", ",")
         num_list = numbers.split(",")
         total = 0
+        contains_negative = False
+        negative_numbers = []
         for num in num_list:
-            if num.isdigit():
+            if is_integer_string(num):
                 if int(num) < 0:
-                    raise ValueError(f"[{num}]")
+                    contains_negative = True
+                    negative_numbers.append(num)
                 if int(num) <= 1000:
                     total += int(num)
             else:
+                print(num)
                 raise ValueError("Invalid input")
+        if contains_negative:
+            raise ValueError(f"[{','.join(negative_numbers)}]")
         return total
     if numbers == "":
         return 0
     else:
         return int(numbers)
+
+
+def is_integer_string(num_str: str) -> bool:
+    try:
+        int(num_str)
+        return True
+    except ValueError:
+        return False
